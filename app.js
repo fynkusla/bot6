62d8afb450a0bb4617f08a78328c24c1824523a6
fynkusla <154467865+fynkusla@users.noreply.github.com>
2023-12-22 18:16:45 +0400

    Update app.js

--- app.js
+++ app.js
@@ -11,27 +11,23 @@ let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
 
 btn1.addEventListener("click", function () {
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-}
-else {
-	tg.MainButton.setText("Вывести информацию по овену");
-	item = "1";
-	tg.MainButton.show();
-	tg.sendData(item);
-
-	}
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    } else {
+        tg.MainButton.setText("Вывести информацию по овену");
+        item = "Овен";  // Изменено на соответствующий знак зодиака
+        tg.MainButton.show();
+    }
 });
+
 btn2.addEventListener("click", function () {
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-else {
-	tg.MainButton.setText("Вывести информацию по тельцу");
-	item = "2";
-	tg.MainButton.show();
-	tg.sendData(item);
-}
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    } else {
+        tg.MainButton.setText("Вывести информацию по тельцу");
+        item = "Телец";  // Изменено на соответствующий знак зодиака
+        tg.MainButton.show();
+    }
 });
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
 	tg.sendData(item);
